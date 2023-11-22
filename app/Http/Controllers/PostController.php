11ecc37e38810e60190a9eed2e663cddc9c8ae79
Aure6 <aurelien7112001@gmail.com>
2023-11-22 11:35:56 +0100

    Added PostController routes and views (generated by blackbox.ai )

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,9 @@ class PostController extends Controller
             'posts' => $posts,
         ]);
     }
-
+    /**
+     * Display the specified resource.
+     */
     public function show($id)
     {
         $post = Post::findOrFail($id);
@@ -28,5 +30,53 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post,
         ]);
+    }
+    //à partir d'ici, c'est copié de l'ancien admin postcontroller
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = Post::make();
+        $post->caption = $request->validated()['caption'];
+        // $post->body = $request->validated()['body'];
+        $post->published_at = $request->validated()['published_at'];
+        $post->user_id = Auth::id();
+        $post->save();
+
+        return redirect()->route('posts.index');
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\DashboardController; //TODO A SUPPR
-use App\Http\Controllers\Admin\ArticleController as AdminArticleController; //TODO A SUPPR
+//use App\Http\Controllers\DashboardController; //TODO A SUPPR
+//use App\Http\Controllers\Admin\ArticleController as AdminArticleController; //TODO A SUPPR
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
@@ -27,6 +27,7 @@ Route::get('/', function () {
 // Route::get('/dashboard', function () {
 //     //return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard'); //protégé par l'authentification
+//postcontroller ajouté au dashboard
 Route::get('/dashboard', [PostController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
