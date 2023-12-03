2afb5bb28178e2ab35fde50d1ca83b2c55b22503
Aure6 <aurelien7112001@gmail.com>
2023-12-03 22:24:00 +0100

    Added a comment to indicate the list of posts section (generated by blackbox.ai )

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,6 +26,7 @@
             </div>
         </form>
 
+        {{-- Liste des posts --}}
         <h1 class="font-bold text-xl mb-4">Liste des posts</h1>
         <ul class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
             @foreach ($posts as $post)
